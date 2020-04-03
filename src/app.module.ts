5e74bbda600f08f06f6b5b33e3cac69b18599a28
n0t0wn3d <notowned@tutanota.com>
2020-04-03 08:52:19 +0300

    added the idea module to the main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeasModule } from './idea/idea.module';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+    IdeasModule,
+    MongooseModule.forRoot('mongodb://localhost/ideas',
+    { useNewUrlParser: true },
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
